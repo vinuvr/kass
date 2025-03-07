bbf935ac971757886423f24ca877904c8a233030
Vinu <vinu@netstratum.com>
2025-03-07 18:54:14 +0530

    removed uuid response in the put request

--- src/kass.erl
+++ src/kass.erl
@@ -2,10 +2,10 @@
 %-include("kass.hrl").
 
 -export([put/2
-        ,get/1
-        % ,get/1
-        % ,update/2
-        % ,delete/2]
+         ,get/1
+         % ,get/1
+         % ,update/2
+         % ,delete/2]
         ]).
 
 
@@ -13,20 +13,16 @@ put(TableName, #{uuid := Uuid} = Data) ->
   Query = kass_utils:put_query(TableName, Data),
   case kass_cassandra:query(Query) of
     {ok, void} -> #{uuid => Uuid};
-    Error -> Error  
+    Error -> Error
   end;
 put(TableName, Data) when is_map(Data) ->
-  UUID = kass_utils:uuid(),
   Query = kass_utils:put_query(TableName, Data#{uuid => UUID}),
-  case kass_cassandra:query(Query) of
-    {ok, void} -> #{uuid => UUID};
-    Error -> Error  
-  end.
+  kass_cassandra:query(Query).
 
 get(TableName) ->
-    TableNameStr= kass_utils:to(list, TableName),
-    Query = "SELECT * FROM " ++ TableNameStr ++ ";",
-    kass_utils:kass_get(kass_cassandra:query(Query)).
+  TableNameStr= kass_utils:to(list, TableName),
+  Query = "SELECT * FROM " ++ TableNameStr ++ ";",
+  kass_utils:kass_get(kass_cassandra:query(Query)).
 
 % get(TableName, Key) ->
 %     Query = "SELECT * FROM " ++ TableName ++ " WHERE key = '" ++ Key ++ "';",
